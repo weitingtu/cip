5740b995f95070741eb8dcb18a8678cd65597ae6
Wei-Ting Tu <bluesoul@ms17.hinet.net>
2019-12-11 15:21:27 +0800

    Test skart

--- cip/main.cpp
+++ cip/main.cpp
@@ -5,8 +5,16 @@
 
 int main()
 {
-	VAMPIRE vampire;
-	vampire.run();
+	//VAMPIRE vampire;
+	//vampire.run();
+
+	double xsd = 2.1;
+	float phi = 0.8f;
+	int iseed = 0;
+
+    Skart s(xsd, phi, iseed);
+	double alpha = 0.2;
+	s.skart_procedure( alpha);
 
 	return 0;
 }
